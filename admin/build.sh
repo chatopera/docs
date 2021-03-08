d51b9e9e8cebdc7583e654dfc8671e75366d4e0f
Hai Liang Wang <hain@chatopera.com>
2021-03-08 18:03:48 +0800

    Omit to generate pdf

--- admin/build.sh
+++ admin/build.sh
@@ -13,5 +13,7 @@ SITE=$baseDir/../dist
 cd $baseDir/../docfx_project
 rm -rf $SITE/_site/*
 # rm -rf obj
-docfx build -o $SITE ./docfx.json 
-docfx pdf -o $SITE
+docfx build -o $SITE ./docfx.json
+# config to generate pdfs
+# https://dotnet.github.io/docfx/tutorial/docfx.exe_user_manual.html#24-generate-pdf-documentation-command-docfx-pdf
+# docfx pdf -o $SITE
