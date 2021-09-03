7149afdb366a7c9f6ca4bb0a4f3f7745267c845b
Hai Liang Wang <hain@chatopera.com>
2021-09-03 08:36:56 +0800

    Update content

---
+++ admin/update.sh
@@ -0,0 +1,15 @@
+#! /bin/bash 
+###########################################
+#
+###########################################
+
+# constants
+baseDir=$(cd `dirname "$0"`;pwd)
+# functions
+
+# main 
+[ -z "${BASH_SOURCE[0]}" -o "${BASH_SOURCE[0]}" = "$0" ] || return
+cd $baseDir/..
+git add --all
+git commit -m "Update content"
+git push origin master
