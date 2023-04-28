2ce760da175716a7d5b30a40a6095f6f756c45a8
Hai Liang Wang <hai@chatopera.com>
2023-04-28 09:46:19 +0800

    https://github.com/chatopera/docs/issues/72 use latest docker image

--- admin/docker/build.sh
+++ admin/docker/build.sh
@@ -10,4 +10,5 @@ baseDir=$(cd `dirname "$0"`;pwd)
 # main 
 [ -z "${BASH_SOURCE[0]}" -o "${BASH_SOURCE[0]}" = "$0" ] || return
 cd $baseDir/../..
+docker pull chatopera/docfx:2.61.0
 docker run --rm --user root --entrypoint /bin/bash -v $PWD:/work chatopera/docfx:2.61.0 -c "/work/admin/build.sh"
