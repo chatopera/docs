2737231647aee882cc4b95ff8eb161cb23d08e18
Hai Liang Wang <hai@chatopera.com>
2023-05-16 17:27:42 +0800

    Fix build script for fast load docker image

--- admin/docker/build.sh
+++ admin/docker/build.sh
@@ -5,10 +5,20 @@
 
 # constants
 baseDir=$(cd `dirname "$0"`;pwd)
+DOCKER_DOCFX_PATH=~/packages/dockers/docker.docfx.tgz
+
 # functions
 
 # main 
 [ -z "${BASH_SOURCE[0]}" -o "${BASH_SOURCE[0]}" = "$0" ] || return
 cd $baseDir/../..
-docker pull chatopera/docfx:2.61.0
+
+if [ -f $DOCKER_DOCFX_PATH ]; then
+    set -x
+    docker load < $DOCKER_DOCFX_PATH
+    set +x
+else
+    docker pull chatopera/docfx:2.61.0
+fi
+
 docker run --rm --user root --entrypoint /bin/bash -v $PWD:/work chatopera/docfx:2.61.0 -c "/work/admin/build.sh"
--- admin/docker/image.sh
+++ admin/docker/image.sh
@@ -13,12 +13,20 @@ cd $baseDir/../..
 imageversion=`git rev-parse --short HEAD`
 
 rm -rf dist/_site/
-
 ./admin/docker/build.sh
 
 if [ ! -d dist/_site/ ]; then
     echo "Fail to generate site."
     exit 1
+fi
+
+DOCKER_NGINX=~/packages/dockers/docker.nginx.tgz
+if [ -f $DOCKER_NGINX ]; then
+    set -x
+    docker load < $DOCKER_NGINX
+    set +x
+else
+    docker pull nginx
 fi
 
 docker build --build-arg VCS_REF=`git rev-parse --short HEAD` \
