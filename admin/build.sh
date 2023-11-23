ece5b54adbb55c54af1d065a80a11ef5a6d9d75f
Hai Liang Wang <hai@chatopera.com>
2023-11-23 10:30:00 +0800

    Add 1st level menu

--- admin/build.sh
+++ admin/build.sh
@@ -17,8 +17,8 @@ DIST=$baseDir/../dist
 CMD_PATH=docfx
 if [ -f /opt/docfx/docfx ]; then
     CMD_PATH="/opt/docfx/docfx"
-elif [ -f /mnt/c/Users/Administrator/.dotnet/tools/docfx.exe ]; then
-    CMD_PATH="/mnt/c/Users/Administrator/.dotnet/tools/docfx.exe"
+elif [ -f /c/Users/Administrator/.dotnet/tools/docfx.exe ]; then
+    CMD_PATH="/c/Users/Administrator/.dotnet/tools/docfx.exe"
 fi
 $CMD_PATH --version
 
